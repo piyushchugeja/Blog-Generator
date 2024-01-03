b7c210215444a0d6697201daf3bbc09fb86da49a
Piyush Chugeja <piyushchugeja@gmail.com>
2024-01-03 23:31:44 +0530

    add language and readability

--- app.py
+++ app.py
@@ -19,19 +19,20 @@ col1, col2 = st.columns([5, 5])
 
 with col1:
     num_words = st.number_input("Number of words", min_value=50, max_value=500, value=100, step=50)
+    language = st.selectbox("Choose language", ["English", "Hindi", "Marathi", "French", "German", "Spanish", "Italian", "Portuguese", "Dutch", "Russian", "Japanese", "Korean", "Chinese"])
 
 with col2:
     blog_style = st.selectbox("Choose blog style", ["Modern", "Classic", "Formal", "Casual", "Futuristic", "Research"])
+    readability = st.selectbox("Choose readability", ["Easy", "Medium", "Hard"])
 
 submit = st.button("Generate blog")
 
 if submit:
-    prompt = "Write a blog on the topic of " + topic + " in " + blog_style + " style. The blog should be " + str(num_words) + " words long."
+    prompt = "Write a blog post about " + topic + " in " + language + " in a " + blog_style + " style that is " + readability + " to read. The blog should be " + str(num_words) + " words long."
     with st.spinner("Generating blog..."):
         try:
             response = get_response(prompt)
             st.success("Blog generated!")
-            st.subheader("Blog - " + topic)
             st.write(response)
         except:
             st.error("Something went wrong. Please try again.")
